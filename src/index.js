b8ff1b6bdcfcbcea6bae4cf55700c7bfbdb5aac4
Kus Cámara <kcmr@users.noreply.github.com>
2019-12-02 00:07:08 +0100

    read config from prefs, config files and save user prefs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 const {Command, flags} = require('@oclif/command')
 const {prompt} = require('enquirer')
+const {cosmiconfig} = require('cosmiconfig')
+const explorer = cosmiconfig('lit-component')
+const Conf = require('conf')
 
 class CreateLitComponentCommand extends Command {
+  get name() {
+    return 'lit-component'
+  }
+
   async run() {
+    // read config found in files (.*rc, package.json, etc.)
+    const configInFiles = await explorer.search(this.name)
+    const localConfig = configInFiles ? configInFiles.config : {}
+
+    // override local config with flags (always have precedence)
     const {flags} = this.parse(CreateLitComponentCommand)
-    const paramNotInFlags = entry => !Object.keys(flags).includes(entry.name)
+    const config = Object.assign(localConfig, flags)
+    // this.log('config', config)
+
+    // read config found in user preferences saved from previous executions
+    const userPrefs = new Conf()
+    const commandPrefs = userPrefs.get(this.name) || {}
+
+    // check if we have all the required params
+    const missingParams = entry => !Object.keys(config).includes(entry.name)
     const questions = [
       {
         type: 'input',
@@ -15,22 +35,31 @@ class CreateLitComponentCommand extends Command {
         type: 'input',
         name: 'scope',
         message: 'Scope for the npm package',
+        default: commandPrefs.scope,
       },
-    ].filter(paramNotInFlags)
+    ].filter(missingParams)
 
     let responses = {}
+
+    // if we have missing params, ask for them
     if (questions.length > 0) {
       responses = await prompt(questions)
     }
-    const options = Object.assign(flags, responses)
 
-    this.log(options)
+    // mix the user responses with the params
+    const options = Object.assign(config, responses)
+
+    // store the input for later usage
+    userPrefs.set(`${this.name}.scope`, options.scope)
+
+    this.log('config', options)
+
   }
 }
 
-CreateLitComponentCommand.description = `Describe the command here
+CreateLitComponentCommand.description = `Creates a LitElement Web Component using @open-wc recommendations
 ...
-Extra documentation goes here
+This is an example for a CLI workshop
 `
 
 CreateLitComponentCommand.flags = {
